43739b401d265ecf0f9390dae008bb0d891a37b2
AlexanderAndersson <aaandersson98@gmail.com>
2023-10-23 12:05:05 +0200

    Initial Commit

---
+++ OnDeviceUppgift4/OnDeviceUppgift4App.swift
@@ -0,0 +1,17 @@
+//
+//  OnDeviceUppgift4App.swift
+//  OnDeviceUppgift4
+//
+//  Created by Alexander Andersson on 2023-10-23.
+//
+
+import SwiftUI
+
+@main
+struct OnDeviceUppgift4App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
